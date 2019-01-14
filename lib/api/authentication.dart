189227a94a573753cc9cf688e9570644567844c6
pie <ibrokemypie@bastardi.net>
2019-01-15 03:27:26 +1100

    Get app id and secret for misskey

--- lib/api/authentication.dart
+++ lib/api/authentication.dart
@@ -1,24 +1,43 @@
 import 'package:fedi/fragments/instance.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:fedi/fragments/shared.dart';
 
 instanceLogin(String instanceUrl) async {
   Instance instance = await Instance.fromUrl(instanceUrl);
-  print(instance.toJson());
   switch (instance.type) {
     case "misskey":
       {
-        final response = await http.get(instance.uri + "/api/v1/instance");
-
-        if (response.statusCode == 200) {
-          // If server returns an OK response, parse the JSON
-        } else {
-          throw Exception('Failed to load post');
-        }
+        await misskeyAuth(instance);
         break;
       }
     default:
       {
         throw Exception(instance.type + " isnt supported lol");
       }
+  }
+}
+
+misskeyAuth(Instance instance) async {
+  String actionPath = "/api/app/create";
+  Map<String, dynamic> params = Map.from({
+    "name": appName,
+    "description": appDescription,
+    "callbackUrl": appCallbackUri,
+    "website": appHomepage,
+    "permission": misskeyScope
+  });
+
+  final response =
+      await http.post(instance.uri + actionPath, body: json.encode(params));
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> returned = json.decode(response.body);
+    String appId = returned["id"];
+    String appSecret = returned["secret"];
+    print(appId);
+    print(appSecret);
+  } else {
+    throw Exception('Failed to load post');
   }
 }
---
+++ lib/fragments/shared.dart
@@ -0,0 +1,28 @@
+String appName = "fedi";
+String appDescription = "Mobile client for Mastodon and Miskkey APIs";
+String appCallbackUri = "fedi://appredirect";
+String appHomepage = "https://boopsnoot.gq";
+
+List<String> misskeyScope = [
+    "account-read",
+    "account-write",
+    "note-read",
+    "note-write",
+    "reaction-read",
+    "reaction-write",
+    "following-read",
+    "following-write",
+    "drive-read",
+    "drive-write",
+    "notification-read",
+    "notification-write",
+    "favorite-read",
+    "favorites-read",
+    "favorite-write",
+    "account/read",
+    "account/write",
+    "messaging-read",
+    "messaging-write",
+    "vote-read",
+    "vote-write"
+  ];
