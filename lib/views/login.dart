4b5d0bd50403ef4aa22c5d937e50401f818d1665
pie <ibrokemypie@bastardi.net>
2019-01-14 23:59:21 +1100

    Disable login buttonn when running

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:validators/validators.dart';
 import 'package:fedi/api/authentication.dart';
-import 'package:fedi/fragments/instance.dart';
 
 class LogIn extends StatefulWidget {
   final Function setauth;
@@ -15,6 +14,34 @@ class LogInState extends State<LogIn> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   String _instance;
+  var _clickLogin;
+
+  void _loginAction() async {
+    if (_formKey.currentState.validate()) {
+      setState(() {
+        _clickLogin = null;
+      });
+      _formKey.currentState.save();
+      try {
+        await instanceLogin(this._instance);
+        widget.setauth(true);
+      } catch (e) {
+        print(e.toString());
+        _scaffoldKey.currentState.showSnackBar(SnackBar(
+          content: Text(e.toString()),
+        ));
+      }
+      setState(() {
+        _clickLogin = _loginAction;
+      });
+    }
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _clickLogin = _loginAction;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,22 +69,7 @@ class LogInState extends State<LogIn> {
                   this._instance = value;
                 }),
             RaisedButton(
-              onPressed: () async {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                  try {
-                    await instanceLogin(this._instance);
-                    widget.setauth(true);
-                  } catch (e) {
-                    print(e.toString());
-                    _scaffoldKey.currentState.showSnackBar(SnackBar(
-                      content: Text(e.toString()),
-                    ));
-                  }
-                }
-              },
+              onPressed: _clickLogin,
               child: Text('Submit'),
             ),
           ]),
