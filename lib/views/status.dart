b4ba409366ec3aa8de9750ba3dc9e20baa7f9299
pie <ibrokemypie@bastardi.net>
2019-01-27 03:03:01 +1100

    move renote back into status.dart and move each chunk nto reusable widgets

--- lib/views/status.dart
+++ lib/views/status.dart
@@ -72,6 +72,172 @@ class StatusBuilderState extends State<StatusBuilder> {
     super.initState();
   }
 
+  _buttonRow() => Row(
+        children: <Widget>[
+          // Reply
+          Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.reply),
+              onPressed: _reply,
+            ),
+            Text("0"),
+          ]),
+
+          // Boost
+          Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.repeat),
+              onPressed: _renote,
+            ),
+            Text(widget.status.renoteCount.toString()),
+          ]),
+
+          // Favourite
+          Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.star),
+              onPressed: _toggleFavourite,
+              color: favouriteColour,
+            ),
+            Text(widget.status.favCount.toString()),
+          ]),
+        ],
+      );
+
+// TODO: content warning
+  _bodyText(String bodyText) => Container(
+        padding: const EdgeInsets.only(bottom: 8.0, right: 32.0),
+        child: Text(bodyText),
+      );
+
+// TODO: sensitive media
+  _files(Widget files) => Container(
+        padding: const EdgeInsets.only(bottom: 8.0, right: 32.0),
+        child: files,
+      );
+
+  _accountName(String name) => Text(name);
+
+  _nickName(String nick) => Container(
+        padding: const EdgeInsets.only(right: 8.0),
+        child: Text(
+          nick,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+
+  _visibilityIcon() => Container(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: Icon(visIcon(widget.status.visibility), size: 16.0));
+
+  _avatar(String avatarUrl) => Container(
+        alignment: FractionalOffset.topCenter,
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8),
+        child: CircleAvatar(
+          backgroundImage: new CachedNetworkImageProvider(avatarUrl),
+        ),
+      );
+
+  _authorRow(String nickName, String accountName, Widget trailing) => Container(
+        padding: const EdgeInsets.only(bottom: 8.0, top: 8),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _nickName(nickName),
+                    _accountName(accountName),
+                  ]),
+              trailing,
+            ]),
+      );
+
+  _divider() => Divider(
+        height: 4,
+      );
+
+  _renotedBy() => Expanded(
+          child: Row(
+        children: <Widget>[
+          Icon(Icons.repeat),
+          Text("renoted by " + widget.status.author.nickname),
+        ],
+      ));
+
+  _renoteRow() => Material(
+        elevation: 1,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          color: Colors.red,
+          child: Row(
+            children: <Widget>[
+              Container(
+                alignment: FractionalOffset.topCenter,
+                padding: const EdgeInsets.only(left: 16.0, right: 4),
+                child: CircleAvatar(
+                  radius: 16,
+                  backgroundImage: new CachedNetworkImageProvider(
+                      widget.status.author.avatarUrl),
+                ),
+              ),
+              _renotedBy(),
+              _visibilityIcon()
+            ],
+          ),
+        ),
+      );
+
+  _statusTile() => <Widget>[
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _avatar(widget.status.author.avatarUrl),
+
+            // Content
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _authorRow(widget.status.author.nickname,
+                    widget.status.author.acct, _visibilityIcon()),
+                _bodyText(widget.status.body),
+                _files(widget.status.statusFiles()),
+                _buttonRow(),
+              ],
+            )),
+          ],
+        ),
+        _divider(),
+      ];
+
+  _renoteTile() => <Widget>[
+        _renoteRow(),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _avatar(widget.status.renote.author.avatarUrl),
+
+            // Content
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _authorRow(widget.status.renote.author.nickname,
+                    widget.status.renote.author.acct, Container()),
+                _bodyText(widget.status.renote.body),
+                _files(widget.status.renote.statusFiles()),
+                _buttonRow(),
+              ],
+            )),
+          ],
+        ),
+        _divider(),
+      ];
+
   @override
   Widget build(BuildContext context) {
     setState(() {
@@ -82,114 +248,10 @@ class StatusBuilderState extends State<StatusBuilder> {
       }
     });
 
-    Widget statusTile;
-    statusTile = Container(
-        child: Column(
-      children: <Widget>[
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // Avatar
-            Container(
-              alignment: FractionalOffset.topCenter,
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8),
-              child: CircleAvatar(
-                backgroundImage: new CachedNetworkImageProvider(
-                    widget.status.author.avatarUrl),
-              ),
-            ),
-
-            // Content
-            Expanded(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                // Author
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8.0, top: 8),
-                  child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              // Nickname
-                              Container(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: Text(
-                                  widget.status.author.nickname,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-
-                              // Account name
-                              Text(widget.status.author.acct),
-                            ]),
-                        Container(
-                            padding: const EdgeInsets.only(right: 16.0),
-                            child: Icon(visIcon(widget.status.visibility),
-                                size: 16.0))
-                      ]),
-                ),
-
-// TODO: content warning
-                // Body
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8.0, right: 32.0),
-                  child: Text(widget.status.body),
-                ),
-
-// TODO: sensitive media
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8.0, right: 32.0),
-                  child: widget.status.statusFiles(),
-                ),
-
-                // Buttons
-                Row(
-                  children: <Widget>[
-                    // Reply
-                    Row(children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.reply),
-                        onPressed: _reply,
-                      ),
-                      Text("0"),
-                    ]),
-
-                    // Boost
-                    Row(children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.repeat),
-                        onPressed: _renote,
-                      ),
-                      Text(widget.status.renoteCount.toString()),
-                    ]),
-
-                    // Favourite
-                    Row(children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.star),
-                        onPressed: _toggleFavourite,
-                        color: favouriteColour,
-                      ),
-                      Text(widget.status.favCount.toString()),
-                    ]),
-                  ],
-                ),
-              ],
-            )),
-          ],
-        ),
-        Divider(
-          height: 4,
-        ),
-      ],
-    ));
-
-    return statusTile;
+    if (widget.status.renote != null) {
+      return Container(child: Column(children: _renoteTile()));
+    } else {
+      return Container(child: Column(children: _statusTile()));
+    }
   }
 }
