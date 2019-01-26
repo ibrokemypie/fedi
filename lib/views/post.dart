c3b659226033a0fbcf6a82bafdfd2d97dbf2f0d8
pie <ibrokemypie@bastardi.net>
2019-01-27 01:00:51 +1100

    move bottom bar from post view keyboard shown

--- lib/views/post.dart
+++ lib/views/post.dart
@@ -106,114 +106,117 @@ class PostState extends State<Post> {
   Widget build(BuildContext context) {
     return Form(
         child: Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: Column(children: <Widget>[
-        contentWarningField,
-        SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: FormField(
-                builder: (FormFieldState<int> state) => TextField(
-                      autofocus: true,
-                      maxLength: widget.instance.maxChars,
-                      controller: textController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: maxLines,
-                      onChanged: textUpdated,
-                      decoration: InputDecoration(
-                          border: InputBorder.none, hintText: 'Write away!'),
-                    )))
-      ]),
-      bottomNavigationBar: BottomAppBar(
-          color: Colors.red,
-          child: Container(
-              child: Row(
-            children: <Widget>[
-              Container(
-                  padding: const EdgeInsets.only(left: 16, right: 4),
-                  child: RaisedButton(
-                    child: Icon(
-                      Icons.image,
-                      size: 16,
-                    ),
-                    // TODO: media uploading
-                    onPressed: () => {},
-                  )),
-              Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 4),
-                  child: PopupMenuButton<String>(
-                    child: Material(
-                      elevation: 3,
-                      type: MaterialType.button,
-                      color: Colors.blue,
-                      child: Container(
-                        child: Icon(
-                          visIcon(visibility),
-                          size: 16,
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 16),
-                      ),
-                    ),
-                    itemBuilder: (BuildContext context) =>
-                        <PopupMenuEntry<String>>[
-                          const PopupMenuItem<String>(
-                            value: "public",
-                            child: Text('Public'),
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+            body: Column(children: <Widget>[
+              contentWarningField,
+              SingleChildScrollView(
+                  padding: const EdgeInsets.all(16),
+                  child: FormField(
+                      builder: (FormFieldState<int> state) => TextField(
+                            autofocus: true,
+                            maxLength: widget.instance.maxChars,
+                            controller: textController,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: maxLines,
+                            onChanged: textUpdated,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Write away!'),
+                          )))
+            ]),
+            bottomNavigationBar: Transform.translate(
+                offset:
+                    Offset(0.0, -1 * MediaQuery.of(context).viewInsets.bottom),
+                child: BottomAppBar(
+                    color: Colors.red,
+                    child: Container(
+                        child: Row(
+                      children: <Widget>[
+                        Container(
+                            padding: const EdgeInsets.only(left: 16, right: 4),
+                            child: RaisedButton(
+                              child: Icon(
+                                Icons.image,
+                                size: 16,
+                              ),
+                              // TODO: media uploading
+                              onPressed: () => {},
+                            )),
+                        Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            child: PopupMenuButton<String>(
+                              child: Material(
+                                elevation: 3,
+                                type: MaterialType.button,
+                                color: Colors.blue,
+                                child: Container(
+                                  child: Icon(
+                                    visIcon(visibility),
+                                    size: 16,
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 16),
+                                ),
+                              ),
+                              itemBuilder: (BuildContext context) =>
+                                  <PopupMenuEntry<String>>[
+                                    const PopupMenuItem<String>(
+                                      value: "public",
+                                      child: Text('Public'),
+                                    ),
+                                    const PopupMenuItem<String>(
+                                      value: "home",
+                                      child: Text('Home'),
+                                    ),
+                                    const PopupMenuItem<String>(
+                                      value: "followers",
+                                      child: Text('Followers only'),
+                                    ),
+                                    const PopupMenuItem<String>(
+                                      value: "specified",
+                                      child: Text('Direct'),
+                                    ),
+                                  ],
+                              onSelected: _setVisibility,
+                            )),
+                        Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            child: RaisedButton(
+                              child: Text(
+                                "CW",
+                                style: TextStyle(fontSize: 10),
+                              ),
+                              onPressed: _toggleContentWarning,
+                            )),
+                        Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            child: RaisedButton(
+                              child: Icon(
+                                Icons.face,
+                                size: 16,
+                              ),
+                              // TODO: emoji selection
+                              onPressed: () => {},
+                            )),
+                        Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: Text(chars.toString() +
+                                "/" +
+                                widget.instance.maxChars.toString())),
+                        Spacer(),
+                        Container(
+                          padding: const EdgeInsets.only(right: 16),
+                          child: RaisedButton(
+                            child: Text("Submit"),
+                            onPressed: submitAction,
                           ),
-                          const PopupMenuItem<String>(
-                            value: "home",
-                            child: Text('Home'),
-                          ),
-                          const PopupMenuItem<String>(
-                            value: "followers",
-                            child: Text('Followers only'),
-                          ),
-                          const PopupMenuItem<String>(
-                            value: "specified",
-                            child: Text('Direct'),
-                          ),
-                        ],
-                    onSelected: _setVisibility,
-                  )),
-              Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 4),
-                  child: RaisedButton(
-                    child: Text(
-                      "CW",
-                      style: TextStyle(fontSize: 10),
-                    ),
-                    onPressed: _toggleContentWarning,
-                  )),
-              Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 4),
-                  child: RaisedButton(
-                    child: Icon(
-                      Icons.face,
-                      size: 16,
-                    ),
-                    // TODO: emoji selection
-                    onPressed: () => {},
-                  )),
-              Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Text(chars.toString() +
-                      "/" +
-                      widget.instance.maxChars.toString())),
-              Spacer(),
-              Container(
-                padding: const EdgeInsets.only(right: 16),
-                child: RaisedButton(
-                  child: Text("Submit"),
-                  onPressed: submitAction,
-                ),
-              )
-            ],
-          ))),
-    ));
+                        )
+                      ],
+                    ))))));
   }
 }
