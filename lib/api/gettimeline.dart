2543ed18440a90a599fb372b50c089fd3c9a900f
pie <ibrokemypie@bastardi.net>
2019-01-31 21:14:08 +1100

    get mastodon timelines

--- lib/api/gettimeline.dart
+++ lib/api/gettimeline.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 import 'package:fedi/definitions/item.dart';
+import 'package:fedi/definitions/shared.dart';
 
 Future<List<Item>> getTimeline(Instance instance, String authCode, String timelineName,
     {List<Item> currentStatuses, String sinceId}) async {
@@ -19,7 +20,9 @@ Future<List<Item>> getTimeline(Instance instance, String authCode, String timeli
     // TODO: get Publictimeline on mastodon
     default:
       {
-        throw Exception(instance.type + " isnt supported lol");
+        statuses = await getMastodonTimeline(instance, authCode, timelineName,
+            currentStatuses: currentStatuses, sinceId: sinceId);
+        break;
       }
   }
   return statuses;
@@ -59,6 +62,61 @@ Future<List> getMisskeyTimeline(Instance instance, String authCode, String timel
 
   final response =
       await http.post(instance.uri + actionPath, body: json.encode(params));
+
+  if (response.statusCode == 200) {
+    List<dynamic> returned = json.decode(response.body);
+
+    returned.forEach((v) {
+        var status = Item.fromMisskey(v, instance);
+        if (status != null)
+          newStatuses.add(status);
+    });
+
+    if (currentStatuses != null) {
+      return new List<Item>.from(newStatuses)..addAll(currentStatuses);
+    }
+
+    return newStatuses;
+  } else {
+    throw Exception('Failed to load post ' +
+        (instance.uri + actionPath + json.encode(params)));
+  }
+}
+
+Future<List> getMastodonTimeline(Instance instance, String authCode, String timelineName,
+    {List<Item> currentStatuses, String sinceId}) async {
+  List<Item> newStatuses = new List();
+  Map<String, dynamic> params;
+    String actionPath;
+
+
+  if (sinceId == null) {
+    params = Map.from({
+      "limit": "40",
+    });
+  } else {
+    params = Map.from({
+      "limit": "40",
+      "since_id": sinceId,
+    });
+  }
+
+  switch (timelineName) {
+    case "home":
+      actionPath = "/api/v1/timelines/home";
+      break;
+    case "local":
+      actionPath = "/api/v1/timelines/public";
+      params.addAll({"local":"true"});
+      break;
+    case "public":
+      actionPath = "/api/v1/timelines/public";
+      break;
+  }
+
+
+  final response =
+      await http.get(instance.uri + actionPath + "?" + uriEncodeMap(params), headers: {"Authorization":"bearer "+authCode});
 
   if (response.statusCode == 200) {
     List<dynamic> returned = json.decode(response.body);
