a066eb4658cc387d9c276cee0843baff165bee42
pie <ibrokemypie@bastardi.net>
2019-01-31 18:48:59 +1100

    add scopes for mastodon

--- lib/definitions/shared.dart
+++ lib/definitions/shared.dart
@@ -4,25 +4,27 @@ String appCallbackUri = "fedi://appredirect";
 String appHomepage = "https://boopsnoot.gq";
 
 List<String> misskeyScope = [
-    "account-read",
-    "account-write",
-    "note-read",
-    "note-write",
-    "reaction-read",
-    "reaction-write",
-    "following-read",
-    "following-write",
-    "drive-read",
-    "drive-write",
-    "notification-read",
-    "notification-write",
-    "favorite-read",
-    "favorites-read",
-    "favorite-write",
-    "account/read",
-    "account/write",
-    "messaging-read",
-    "messaging-write",
-    "vote-read",
-    "vote-write"
-  ];
+  "account-read",
+  "account-write",
+  "note-read",
+  "note-write",
+  "reaction-read",
+  "reaction-write",
+  "following-read",
+  "following-write",
+  "drive-read",
+  "drive-write",
+  "notification-read",
+  "notification-write",
+  "favorite-read",
+  "favorites-read",
+  "favorite-write",
+  "account/read",
+  "account/write",
+  "messaging-read",
+  "messaging-write",
+  "vote-read",
+  "vote-write"
+];
+
+List<String> mastodonScope = ["read", "write", "follow", "push"];
