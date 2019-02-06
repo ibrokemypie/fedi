5bde4e9a33e4e98bd8ddeb85d25ee8e6ee24c61d
pie <ibrokemypie@bastardi.net>
2019-02-06 21:28:35 +1100

    add relationship api

---
+++ lib/api/relationship.dart
@@ -0,0 +1,72 @@
+import 'package:fedi/definitions/instance.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'package:fedi/definitions/relationship.dart';
+
+Future<Relationship> getRelationship(
+    Instance instance, String authCode, String userId) async {
+  Relationship relationship;
+
+  switch (instance.type) {
+    case "misskey":
+      {
+        relationship = await getMisskeyRelationship(instance, authCode, userId);
+        break;
+      }
+    case "mastodon":
+      {
+        relationship =
+            await getMastodonRelationship(instance, authCode, userId);
+        break;
+      }
+    default:
+      {
+        throw (instance.type + "not supported");
+      }
+  }
+  return relationship;
+}
+
+Future<Relationship> getMisskeyRelationship(
+    Instance instance, String authCode, String userId) async {
+  Map<String, dynamic> params;
+  String actionPath = "/api/users/relation";
+
+  params = Map.from({
+    "i": authCode,
+    "userId": userId,
+  });
+
+  final response =
+      await http.post(instance.uri + actionPath, body: json.encode(params));
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> returned = json.decode(response.body);
+
+    return Relationship.fromMisskey(returned);
+  } else {
+    throw Exception('Failed to load post ' +
+        (instance.uri + actionPath + json.encode(params)));
+  }
+}
+
+Future<Relationship> getMastodonRelationship(
+    Instance instance, String authCode, String userId) async {
+  Map<String, dynamic> params;
+  String actionPath = "/api/v1/accounts/relationships";
+
+  params = Map.from({"id": userId});
+
+  final response = await http.post(instance.uri + actionPath,
+      body: params, headers: {"Authorization": "bearer " + authCode});
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> returned = json.decode(response.body);
+
+    return Relationship.fromMastodon(returned);
+  } else {
+    throw Exception('Failed to load post ' +
+        (instance.uri + actionPath + json.encode(params)));
+  }
+}
